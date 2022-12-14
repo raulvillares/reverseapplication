ab04eb8aa554cec42ad540e2862b8f59b5bd5732
Raúl Villares <raulvillares@gmail.com>
2022-12-14 15:20:57 +0100

    foo

--- ReverseAPI/Reverser.cs
+++ ReverseAPI/Reverser.cs
@@ -4,6 +4,7 @@ public class Reverser
 {
     public string Reverse(string? text)
     {
+        
         return !string.IsNullOrEmpty(text) ? new string(text.Reverse().ToArray()) : string.Empty;
     }
 }
